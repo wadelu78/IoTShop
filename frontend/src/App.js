45043a5b3296a7b6241e48cd58cb76d1cb65d8b0
Wei Lu <wadelu0714@gmail.com>
2021-03-07 23:35:04 +1300

    Add user profile screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
 import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
         <Container>
           <Route path='/login' component={LoginScreen} />
           <Route path='/register' component={RegisterScreen} />
+          <Route path='/profile' component={ProfileScreen} />
           <Route path='/product/:id' component={ProductScreen} />
           {/* Any parameter that ends with a question mark will be treated as optional, here, the parameter id is optional */}
           <Route path='/cart/:id?' component={CartScreen} />
