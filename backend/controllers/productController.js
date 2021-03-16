e1508e5ff5dc2de845381744d71e2d3c2d4222f7
Wei Lu <wadelu0714@gmail.com>
2021-03-16 23:31:02 +1300

    Add user review logic

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -90,10 +90,50 @@ const updateProduct = asyncHandler(async (req, res) => {
   }
 })
 
+// @desc    Create new product
+// @route   POST /api/products/:id/reviews
+// @access  Private
+const createProductReview = asyncHandler(async (req, res) => {
+  const { rating, comment } = req.body
+
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    const alreadyReviewed = product.reviews.find(
+      (r) => r.user.toString() === req.decodedId.toString()
+    )
+    if (alreadyReviewed) {
+      res.status(400)
+      throw new Error('Product already reviewed')
+    }
+    const review = {
+      //TODO: change the logic from getting a user name from req.decodedId
+      name: 'req.user.name',
+      rating: Number(rating),
+      comment,
+      user: req.decodedId,
+    }
+
+    product.reviews.push(review)
+
+    product.numReviews = product.reviews.length
+
+    product.rating =
+      product.reviews.reduce((acc, item) => item.rating + acc, 0) /
+      product.reviews.length
+
+    await product.save()
+    res.status(201).json({ message: 'Review added' })
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
 export {
   getProducts,
   getProductById,
   deleteProduct,
   createProduct,
   updateProduct,
+  createProductReview,
 }
