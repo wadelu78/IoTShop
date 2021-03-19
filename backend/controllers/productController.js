38511e6a52b8d56fc8e629d5f7dfc42a6f2bfb86
Wei Lu <wadelu0714@gmail.com>
2021-03-19 22:46:15 +1300

    Add search product logic

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,7 +5,18 @@ import Product from '../models/productModel.js'
 // @route   GET /api/products
 // @access  Public
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({})
+  console.log('keyword?', req.query.keyword)
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: 'i',
+        },
+      }
+    : {}
+
+  console.log(keyword)
+  const products = await Product.find({ ...keyword })
   res.json(products)
 })
 
